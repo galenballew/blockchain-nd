6cbfa865c1be93aae0f2f0f4c2c8e6a5c375096c
Galen Ballew <galen.ballew@gmail.com>
2018-10-06 16:21:26 -0500

    removed leveldb script and folded functions into Blockchain class. Class also consolidated getBlock and getBlockHeight because it was redundant. all Blockchain methods are now asynchronous. utiilized proper Promise.prototype.then(); methods in index.js

---
+++ private_blockchain/blockchain.js
@@ -0,0 +1,148 @@
+/* ===== SHA256 with Crypto-js ===============================
+|  Learn more: Crypto-js: https://github.com/brix/crypto-js  |
+|  =========================================================*/
+
+const SHA256 = require('crypto-js/sha256');
+const db = require('level')('./chaindata');
+const Block = require('./payload');
+
+
+/* ===== Blockchain Class ==========================
+|  Class with a constructor for new blockchain 		|
+|  ================================================*/
+class Blockchain{
+
+	constructor(){
+		/* Instantiates Blockchain
+		* If there is nothing in ./chaindata, it adds a Block to the blockchain
+		* input: 
+		* return:
+		*/ 
+		this.getBlockHeight().then((height) => {
+			if (height === -1) {
+				this.addBlock(new Block("Genesis block")).then(() => console.log("Blockchain initialized with genesis block."));
+			}
+		});
+	}
+
+	async addBlock(newBlock){
+		/* adds a block to the blockchain
+		* input: Block [Object] (./payload.js)
+		* return: 
+		*/
+		const height = parseInt(await this.getBlockHeight());
+		newBlock.height = height + 1;
+		newBlock.time = new Date().getTime().toString().slice(0, -3);
+
+		if (newBlock.height > 0) {
+			const previousBlock = await this.getBlock(height);
+			newBlock.previousBlockHash = previousBlock.hash;
+		}
+
+		newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
+		await this.addBlocktoDB(newBlock.height, JSON.stringify(newBlock));
+  }
+
+  async validateBlock(blockHeight){
+		/* validates a single block by checking stored hash vs computed hash
+		* input: blockHeight [int]
+		* return: [Boolean]
+		*/
+		const block = await this.getBlock(blockHeight);
+		const currentHash = block.hash;
+		block.hash = "";
+		const validHash = SHA256(JSON.stringify(block)).toString();
+
+		if(currentHash === validHash) {
+			return true;
+		} else {
+			console.log("Block #${blockHeight} is invalid.");
+			return false;
+		}
+	}
+
+	async validateChain(){
+		/* validates blockchain by validating all blocks
+		* input: 
+		* return: [Boolean]
+		*/
+		let previousBlockHash = "";
+		const errorLog = [];
+		const chainHeight = await this.getBlockHeight();
+
+		for (let i=0; i < chainHeight; i++) {
+			let blockValid = await this.getBlock(i).then(async function(block) {
+				await this.validateBlock(block.height);
+			});
+			if (!blockValid) {
+				errorLog.push(i);
+			}
+			// equates for genesis block
+			if (block.previousBlockHash !== previousBlockHash) {
+				errorLog.push(i);
+			}
+			previousBlockHash = block.hash;
+		}
+		if (errorLog.length > 0) {
+			console.log("The following blocks are invalid: ${errorLog}");
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+
+	async addBlocktoDB(key, value) {
+		/* adds block to levelDB for persistence 
+		* input: key, value [int, stringified JSON]
+		* return: [Promise]
+		*/
+		return new Promise((resolve, reject) => {
+			db.put(key, value, (error) => {
+				if (error) {
+					reject(error)
+				}
+				console.log(`Added block #${key}`);
+				resolve(key);
+			});
+		});
+	}
+
+	async getBlockHeight() {
+			/* retrieves current heigh of blockchain 
+		* input: 
+		* return: height [int]
+		*/
+		return new Promise((resolve, reject) => {
+			let height = -1;
+			db.createReadStream().on('data', (data) => {
+				height++;
+			}).on('error', (error) => {
+				reject(error);
+			}).on('close', () => {
+				resolve(height);
+			});
+		});
+	}
+
+	async getBlock(key) {
+		/* retrieves current heigh of blockchain 
+		* input: key [int] (blockHeight)
+		* return: block [JSON]
+		*/
+		return new Promise((resolve, reject) => {
+			db.get(key, (error, value) => {
+				if (value === undefined) {
+					reject('Block not found.');
+				} else if (error) {
+					reject(error);
+				}
+				resolve(value);
+			});
+		});
+	}
+}
+
+
+// export for testing
+module.exports = Blockchain;
--- private_blockchain/index.js
+++ private_blockchain/index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const Block = require('./payload');
-const Blockchain = require('./simple');
+const Blockchain = require('./blockchain');
 const chain = new Blockchain();
 
-app.listen(8000, () => console.log('Listening on port 8000'));
+app.listen(8000, () => console.log('Listening on port 8000.'));
 app.use(bodyParser.json());
 app.get('/', (req, res) => res.status(404).json({
   "status": 404,
@@ -13,39 +13,50 @@ app.get('/', (req, res) => res.status(404).json({
 }));
 
 /* ================= GET =============================
+|           Fetch current blockchain height           |
+|  ==================================================*/
+app.get('/block/', async (req, res) => {
+  chain.getBlockHeight().then(function(height){
+    res.send("Current blockchain height: " + height + "\n");
+  }).catch(function(error) {
+  res.status(404).json({
+    "status": 404,
+    "message": "Blockchain is invalid."
+  });
+});
+});
+
+/* ================= GET =============================
 |  Fetch a block by passing block height in payload  |
 |  ==================================================*/
 app.get('/block/:height', async (req, res) => {
-  try {
-    const response = chain.getBlock(req.params.height);
-    res.send(response);
-  } catch (error) {
+    chain.getBlock(req.params.height).then(function(block){
+      res.send(block + "\n");
+    }).catch(function(error) {
     res.status(404).json({
       "status": 404,
       "message": "Block not found."
     });
-  }
+  });
 });
 
 /* ================= POST =============================
 |       Add a new block to the blockchain             |
 |  ===================================================*/
 app.post('/block', async (req, res) => {
-  try {
-    if (req.body.body === '' || req.body.body === undefined) {
-      throw new Error("Payload missing. Include stringified block in request payload.");
-    }
-    chain.addBlock(new Block(req.body.body));
-    // use .then() to make sure it happens in order
-    // otherwise you'll catch an error about height not having resolved yet
-    let response = chain.getBlockHeight().then(function(height) {
-      chain.getBlock(height);
-    });
-    res.status(201).send(response);
-  } catch (error) {
+  if (req.body.body === '' || req.body.body === undefined) {
     res.status(400).json({
       "status": 400,
-      "message": error
+      message: "Block body missing."
     });
   }
+  chain.addBlock(new Block(req.body.body))
+  chain.getBlockHeight().then((height) => {
+    res.send("Block added successfully. New blockchain height: " + height + "\n");
+  }).catch(function(error) {
+    res.status(404).json({
+      "status": 400,
+      "message": "Block not added."
+    });
+  });
 });
